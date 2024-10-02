7354109f78bac8913bab9a83bbcdc9f1eebabd94
Garzzza7 <piotr.czapela@gmail.com>
2024-10-02 19:22:16 +0200

    kitty

--- propagate.sh
+++ propagate.sh
@@ -5,9 +5,10 @@ cp i3/* $HOME/.config/i3/
 cp i3status/* $HOME/.config/i3status/
 cp emacs/* $HOME/.emacs.d/
 cp nvim/* $HOME/.config/nvim/
-cp vim/.vimrc	$HOME/.vimrc
+cp vim/vimrc	$HOME/.vimrc
 cp scripts/* $HOME/scripts/
 cp vscode/settings.json $HOME/.config/Code/User/settings.json
-cp omz/.zshrc $HOME/.zshrc
+cp omz/zshrc $HOME/.zshrc
+cp kitty/* $HOME/.config/kitty/
 cat vscode/ext.txt | xargs -L 1 code --install-extension
 
--- update.sh
+++ update.sh
@@ -2,12 +2,13 @@
 
 cp -r $HOME/.config/i3/* i3/
 cp -r $HOME/.config/nvim/init.lua nvim/
-cp -r $HOME/.vimrc vim/.vimrc
+cp -r $HOME/.vimrc vim/vimrc
 cp -r $HOME/scripts/* scripts/
 cp -r $HOME/.config/i3status/* i3status/
 cp -r $HOME/.emacs.d/* emacs/
 cp -r $HOME/.config/Code/User/settings.json vscode/settings.json
-cp -r $HOME/.zshrc omz/.zshrc
+cp -r $HOME/.zshrc omz/zshrc
+cp -r $HOME/.config/kitty/* kitty/
 code --list-extensions > vscode/ext.txt
 
 
