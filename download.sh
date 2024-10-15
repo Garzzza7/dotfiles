69adaaed73234f824de7104b97027443bee65a16
Garzzza7 <piotr.czapela@gmail.com>
2024-10-15 13:48:21 +0200

    add gh to download

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install npm xclip ripgrep ninja-build gettext cmake unzip curl build-essential git gcc clang toilet kitty htop tree pypy3 kolourpaint mypaint libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config luarocks vim brightnessctl exiftool libx11-dev gdb feh
+sudo apt install npm xclip ripgrep ninja-build gettext cmake unzip curl build-essential git gcc clang toilet kitty htop tree pypy3 kolourpaint mypaint libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config luarocks vim brightnessctl exiftool libx11-dev gdb feh gh
--- propagate.sh
+++ propagate.sh
@@ -7,8 +7,8 @@ cp emacs/* $HOME/.emacs.d/
 cp nvim/* $HOME/.config/nvim/
 cp vim/vimrc	$HOME/.vimrc
 cp scripts/* $HOME/scripts/
-cp vscode/settings.json $HOME/.config/Code/User/settings.json
+# cp vscode/settings.json $HOME/.config/Code/User/settings.json
 cp omz/zshrc $HOME/.zshrc
 cp kitty/* $HOME/.config/kitty/
-cat vscode/ext.txt | xargs -L 1 code --install-extension
+# cat vscode/ext.txt | xargs -L 1 code --install-extension
 
