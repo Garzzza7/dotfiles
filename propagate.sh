4fdd89c77a65d83bf68fa5393be27645aae63288
Piotr Czapla <piotr.czapela@gmail.com>
2024-11-02 16:39:45 +0100

    modifying propagation scripts

--- propagate.sh
+++ propagate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 if [ "$(git pull)" != "Already up to date." ]; then
     echo Changes detected.
-    if [ "$(which i3)" != "" ]; then
+    if [ "$(which i3)" ]; then
         echo I3 found.
         cp i3/* $HOME/.config/i3/
         cp i3status/* $HOME/.config/i3status/
@@ -10,28 +11,28 @@ if [ "$(git pull)" != "Already up to date." ]; then
         echo Omitting I3.
     fi
 
-    if [ "$(which emacs)" != "" ]; then
+    if [ "$(which emacs)" ]; then
         echo Emacs found.
         cp emacs/* $HOME/.emacs.d/
     else
         echo Omitting Emacs.
     fi
 
-    if [ "$(which nvim)" != "" ]; then
+    if [ "$(which nvim)" ]; then
         echo Neovim found.
         cp nvim/* $HOME/.config/nvim/
     else
         echo Omitting Neovim.
     fi
 
-    if [ "$(which vim)" != "" ]; then
+    if [ "$(which vim)" ]; then
         echo Vim found.
         cp vim/vimrc	$HOME/.vimrc
     else
         echo Omitting Vim.
     fi
 
-    if [ "$(which code)" != "" ]; then
+    if [ "$(which code)" ]; then
         echo VSCode found.
         cp vscode/settings.json $HOME/.config/Code/User/settings.json
         cat vscode/ext.txt | xargs -L 1 code --install-extension
@@ -39,28 +40,34 @@ if [ "$(git pull)" != "Already up to date." ]; then
         echo Omitting VSCode.
     fi
 
-    if [ "$(which omz)" != "" ]; then
+    if [ "$(which omz)" ]; then
         echo OMZ found.
         cp omz/zshrc $HOME/.zshrc
     else
         echo Omitting OMZ.
     fi
 
-    if [ "$(which kitty)" != "" ]; then
+    if [ "$(which kitty)" ]; then
         echo Kitty found.
         cp kitty/* $HOME/.config/kitty/
     else
         echo Omitting Kitty.
     fi
 
-    if [ "$(which lazygit)" != "" ]; then
+    if [ "$(which lazygit)" ]; then
         echo Lazygit found.
         cp lazygit/config.yml $HOME/.config/lazygit/config.yml
     else
         echo Omitting Lazygit.
     fi
 
-    cp bash/bashrc $HOME/.bashrc
+    if [ "$(which bash)" ]; then
+        echo Bash found.
+        cp bash/bashrc $HOME/.bashrc
+    else
+        echo Omitting Lazygit.
+    fi
+
     echo Done.
 else
     echo Nothing new to propagate.
