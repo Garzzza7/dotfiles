d74511ec20f5869f849cc3bef7cbd2d8e1f8ca78
Garzzza7 <piotr.czapela@gmail.com>
2025-01-24 17:20:24 +0100

    debloat

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,5 +1,7 @@
 config.load_autoconfig()
-config.source('gruvbox.py')
+# config.source('gruvbox.py')
 c.tabs.background = True
 c.new_instance_open_target = 'window'
 c.downloads.position = 'bottom'
+config.set("colors.webpage.darkmode.enabled", True)
+c.content.javascript.enabled = False
