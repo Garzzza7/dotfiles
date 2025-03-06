162a25186488179bd6b028fd128e3646a19d56c2
Garzzza7 <piotr.czapela@gmail.com>
2025-03-06 10:52:45 +0100

    changes

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,7 @@
-config.load_autoconfig()
+config.load_autoconfig(False)
 # config.source('gruvbox.py')
-c.tabs.background = True
-c.new_instance_open_target = 'window'
-c.downloads.position = 'bottom'
-config.set("colors.webpage.darkmode.enabled", True)
-c.content.javascript.enabled = False
+# c.tabs.background = True
+# c.new_instance_open_target = "window"
+# c.downloads.position = "bottom"
+# config.set("colors.webpage.darkmode.enabled", False)
+# c.content.javascript.enabled = True
---
+++ scripts/rene.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xrandr --output HDMI-0 --auto --same-as eDP-1-1
+pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo
+
+if command -v google-chrome 2>&1 >/dev/null
+then
+    google-chrome https://www.netflix.com/browse &
+elif command -v chromium-browser 2>&1 >/dev/null
+then
+    chromium-browser https://www.netflix.com/browse &
+elif command -v firefox 2>&1 >/dev/null
+then
+    firefox https://www.netflix.com/browse &
+else
+    install_chrome.sh
+    google-chrome https://www.netflix.com/browse &
+fi
