5bd49ad0ac4af62be3b18997b917923b835fee0a
Garzzza7 <piotr.czapela@gmail.com>
2025-03-16 17:09:54 +0000

    typo

--- download.sh
+++ download.sh
@@ -8,9 +8,3 @@ elif [ -f "/etc/debian-version" ]; then
     echo "Debian detected!!!!!!"
     sudo apt install npm xclip ripgrep ninja-build gettext cmake unzip curl build-essential git gcc clang toilet kitty htop tree pypy3 kolourpaint mypaint libffi-dev libffi8 libgmp-dev libgmp10 libncurses-dev pkg-config luarocks vim brightnessctl exiftool libx11-dev gdb feh gh mpv jq uuid libxrandr-dev pulseaudio pulseaudio-utils libpcap0.8-dev flex bison intel-gpu-tools python3-full python3-pip network-manager mupdf mupdf-tools strace valgrind mc alsa-utils libwayland-dev libxkbcommon-dev xorg-dev power-profiles-daemon cinnamon libglu1-mesa-dev libasio-dev llvm python3-bs4 ncal trash-cli qutebrowser fd-find
 fi
-
-if [ -f "/etc/arch-release" ]; then
-    echo "Arch detected!!!!!!"
-elif [ -f "/etc/debian-version" ]; then
-    echo "Debian detected!!!!!!"
-fi
