1e4c855545afb9cba8323b6a4bbfe86d94ec4fec
Garzzza7 <piotr.czapela@gmail.com>
2025-04-02 22:22:01 +0200

    changes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -123,7 +123,7 @@ return {
 
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sgg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 
