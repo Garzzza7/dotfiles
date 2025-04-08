9eab347314bbfadcb86e14fbcefb8aea542db25b
Garzzza7 <piotr.czapela@gmail.com>
2025-04-08 20:06:52 +0200

    syntax

---
+++ nvim/lua/config/filetype.lua
@@ -0,0 +1,21 @@
+-- TODO: make it work
+vim.filetype.add({
+	pattern = {
+		[".v"] = "rocq",
+	},
+})
+vim.filetype.add({
+	pattern = {
+		[".nim"] = "nim",
+	},
+})
+vim.filetype.add({
+	pattern = {
+		[".frag"] = "glsl400",
+	},
+})
+vim.filetype.add({
+	pattern = {
+		[".vert"] = "glsl400",
+	},
+})
