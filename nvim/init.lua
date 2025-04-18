297adbb33c47ac9d70770065e6071a64840ff69b
Garzzza7 <piotr.czapela@gmail.com>
2025-04-18 13:17:47 +0200

    qol

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,27 +2,3 @@ require("config.keymaps")
 require("config.lazy")
 require("config.classic")
 require("config.apperance")
-
--- require("plugins.cmp")
---
--- require("plugins.lazygit")
---
--- require("plugins.telescope")
---
--- require("plugins.vim-sleuth")
---
--- require("plugins.compilemode")
---
--- require("plugins.conform")
---
--- require("plugins.lspconfig")
---
--- require("plugins.toggleterm")
---
--- require("plugins.gitsigns")
---
--- require("plugins.multiplecursors")
---
--- require("plugins.treesitter")
---
--- require("plugins.themes")
