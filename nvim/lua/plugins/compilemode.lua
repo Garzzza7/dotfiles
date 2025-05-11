48c3d66dad380b9efde03d5bd3522e64dcdd7ea5
Garzzza7 <piotr.czapela@gmail.com>
2025-05-11 23:33:55 +0200

    remove grammars

--- nvim/lua/plugins/compilemode.lua
+++ nvim/lua/plugins/compilemode.lua
@@ -1,15 +1,17 @@
 ---@diagnostic disable: undefined-doc-name
 return {
-	"ej-shafran/compile-mode.nvim",
-	tag = "v2.0.0",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "m00qek/baleia.nvim", tag = "v1.3.0" },
+	{
+		"ej-shafran/compile-mode.nvim",
+		tag = "v2.0.0",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "m00qek/baleia.nvim", tag = "v1.3.0" },
+		},
+		config = function()
+			---@type CompileModeOpts
+			vim.g.compile_mode = {
+				baleia_setup = true,
+			}
+		end,
 	},
-	config = function()
-		---@type CompileModeOpts
-		vim.g.compile_mode = {
-			baleia_setup = true,
-		}
-	end,
 }
