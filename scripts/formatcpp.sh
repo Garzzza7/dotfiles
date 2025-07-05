425eccb3e1c64314287ca34ef145c2b6754addac
Garzzza7 <piotr.czapela@gmail.com>
2025-07-06 00:41:53 +0200

    formatting

--- scripts/formatcpp.sh
+++ scripts/formatcpp.sh
@@ -4,3 +4,5 @@
 #     /home/pc/.local/share/nvim/mason/bin/clang-format -i $path
 # done
 $HOME/.local/share/nvim/mason/bin/clang-format -i *.cpp
+$HOME/.local/share/nvim/mason/bin/clang-format -i *.h
+$HOME/.local/share/nvim/mason/bin/clang-format -i *.cc
