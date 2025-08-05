e9088d0484f2ffdd5ce993ed3b8b5f9bd2201512
Garzzza7 <piotr.czapela@gmail.com>
2025-08-05 20:11:12 +0200

    changes

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -67,5 +67,17 @@ return {
 				-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
 			},
 		},
+		vim.keymap.set ("n", "]t", function ()
+			require ("todo-comments").jump_next ()
+		end, { desc = "Next todo comment" }),
+
+		vim.keymap.set ("n", "[t", function ()
+			require ("todo-comments").jump_prev ()
+		end, { desc = "Previous todo comment" }),
+
+		-- Specific type of comment
+		-- vim.keymap.set ("n", "]t", function ()
+		-- 	require ("todo-comments").jump_next ({ keywords = { "ERROR", "WARNING" } })
+		-- end, { desc = "Next error/warning todo comment" }),
 	},
 }
