69845747b4a18369e5c995450c927581bb8c487b
Garzzza7 <piotr.czapela@gmail.com>
2025-08-13 22:06:32 +0200

    changes

---
+++ nvim/lua/plugins/toggler.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"nguyenvukhang/nvim-toggler",
+		version = "v0.3.1",
+		dependencies = {},
+		config = function ()
+			require ("nvim-toggler").setup ({
+				inverses = {
+					["true"] = "false",
+					["on"] = "off",
+					["("] = ")",
+					["{"] = "}",
+					["["] = "]",
+					["function"] = "end",
+				},
+				remove_default_keybinds = true,
+				remove_default_inverses = true,
+				autoselect_longest_match = false,
+			})
+			vim.keymap.set ({ "n", "v", "i" }, "<C-a>", require ("nvim-toggler").toggle, { noremap = true, silent = true, desc = "Toggler" })
+		end,
+	},
+}
