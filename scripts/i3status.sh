80c95f633b1482b88f6cbb728ddc906519b2a11b
Garzzza7 <piotr.czapela@gmail.com>
2025-08-26 14:20:40 +0200

    changes

--- scripts/i3status.sh
+++ scripts/i3status.sh
@@ -2,5 +2,6 @@
 
 i3status -c $HOME/.config/i3status/i3status.conf | while :; do
 	read line
-	echo "idk|$line" || exit 1
+	up="$(uptime | awk '{print $3}')"
+	echo "Uptime: ${up%?}|$line" || exit 1
 done
