91753d7a0467692027324ffc40de54eec410aaf9
Garzzza7 <piotr.czapela@gmail.com>
2025-09-07 02:47:54 +0200

    changes

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
@@ -13,7 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(gnuplot magit rust-mode)))
+ '(package-selected-packages '(company gnuplot magit rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,4 +25,15 @@
  ;; If there is more than one, they won't work right.
  )
 
+(global-company-mode 1)
+(setq company-backends '((company-dabbrev)))
+(setq company-dabbrev-minimum-length 1)
+(setq company-dabbrev-ignore-case 1)
+(setq company-idle-delay nil)
+
+(define-key company-mode-map (kbd "TAB") 'company-complete)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
 (invert-face 'default)
+
