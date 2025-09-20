8cce64905ae8fb57503495c12a701b8b789c04d0
Garzzza7 <piotr.czapela@gmail.com>
2025-09-21 01:18:02 +0200

    changes

--- emacs/init.el
+++ emacs/init.el
@@ -19,8 +19,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company cuda-mode exec-path-from-shell gnuplot lsp-mode lua-mode
-	     magit rust-mode)))
+   '(company cuda-mode exec-path-from-shell gnuplot haskell-mode lsp-mode
+	     lua-mode magit rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
