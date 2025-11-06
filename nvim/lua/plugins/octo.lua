735ebd0ee9b9d3b6fe0a71bb785157fb5fa80b91
Garzzza7 <piotr.czapela@gmail.com>
2025-11-06 10:58:01 +0100

    changes

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -1,5 +1,6 @@
 return {
 	"pwntester/octo.nvim",
+	enabled = false,
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", commit = "857c5ac632080dba10aae49dba902ce3abf91b35" },
 		{ "nvim-telescope/telescope.nvim" },
--- scripts/i3status.sh
+++ scripts/i3status.sh
@@ -2,6 +2,6 @@
 
 i3status -c $HOME/.config/i3status/i3status.conf | while :; do
 	read line
-	up="$(uptime | awk '{print $3}')"
-	echo "Uptime: ${up%?}|$line" || exit 1
+	up="$(uptime | sed -E 's/.* up[[:space:]]+//; s/,.*//')"
+	echo "Uptime: ${up}|${line}" || exit 1
 done
