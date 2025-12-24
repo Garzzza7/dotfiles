cb0a3d01f0816a383c8371a79441a41357190f7b
Garzzza7 <piotr.czapela@gmail.com>
2025-12-24 16:15:13 +0100

    changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,11 +4,6 @@ pcall (require ("config.classic"))
 pcall (require ("config.apperance"))
 pcall (require ("config.external_parsers"))
 
--- vim.api.nvim_create_autocmd ("BufEnter", {
--- 	pattern = "*",
--- 	command = "silent! lcd %:p:h",
--- })
-
 vim.api.nvim_create_autocmd ("BufEnter", {
     pattern = "*.m",
     callback = function () vim.cmd ("set ft=mercury") end,
@@ -22,4 +17,9 @@ vim.api.nvim_create_autocmd ("BufRead", {
 vim.api.nvim_create_autocmd ("BufNewFile", {
     pattern = "*.m",
     callback = function () vim.cmd ("set ft=mercury") end,
+})
+
+vim.api.nvim_create_autocmd ("FileType", {
+    pattern = "mercury",
+    command = "setlocal commentstring=%\\ %s",
 })
