11e5c5932a80ce705989d82025ca74b9b90eed96
Garzzza7 <piotr.czapela@gmail.com>
2026-02-24 22:22:30 +0100

    changes

--- nvim/lua/config/classic.lua
+++ nvim/lua/config/classic.lua
@@ -7,7 +7,7 @@ vim.opt.ignorecase = true
 vim.opt.inccommand = "split"
 vim.opt.list = true
 vim.opt.listchars = { tab = "> ", trail = "_", nbsp = "␣" }
-vim.opt.mouse = ""
+vim.opt.mouse = "n"
 vim.opt.number = false
 vim.opt.relativenumber = false
 vim.opt.scrolloff = 16
