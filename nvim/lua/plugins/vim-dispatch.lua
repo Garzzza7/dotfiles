2e0439cd4c2e9b54c2e68a9686552a9db66dc73c
Garzzza7 <piotr.czapela@gmail.com>
2026-03-02 01:13:02 +0100

    changes

--- nvim/lua/plugins/vim-dispatch.lua
+++ nvim/lua/plugins/vim-dispatch.lua
@@ -1,5 +1,15 @@
 return {
     {
         "tpope/vim-dispatch",
+        config = function ()
+            -- TODO: unscuff this
+            vim.api.nvim_create_autocmd ("BufEnter", {
+                pattern = "*.rs",
+                callback = function () vim.bo.makeprg = "cargo run" end,
+            })
+        end,
+    },
+    {
+        "radenling/vim-dispatch-neovim",
     },
 }
