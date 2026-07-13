7fd541e9c5f643c2ac62761abfc895e5ca2cc7d4
Garzzza7 <piotr.czapela@gmail.com>
2026-07-14 01:21:54 +0200

    changes

--- scripts/comp.sh
+++ scripts/comp.sh
@@ -8,6 +8,8 @@ elif [[ "$suffix" == ".py" ]]; then
     comppy.sh $input
 elif [[ "$suffix" == ".rs" ]]; then
     comprs.sh $input
+elif [[ "$suffix" == ".m" ]]; then
+    compmerc.sh $input
 else
     echo "Unknown language!!!"
 fi
---
+++ scripts/compmerc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+input=$1
+suffix=".m"
+
+if [[ $input == *"$suffix" ]]; then
+    input=${input%$suffix}
+elif [[ $input == *"." ]]; then
+    input=${input%.}
+fi
+
+time mmc -E --make $input -O5
+
+time ./$input <$input.txt
