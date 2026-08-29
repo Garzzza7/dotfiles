3b42b3f533a7da1a7575797db11b1cf665620523
Garzzza7 <piotr.czapela@gmail.com>
2026-08-29 22:40:07 +0200

    new script

---
+++ scripts/sanitize_cpp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+input=$1
+suffix=".cpp"
+if [[ $input == *"$suffix" ]]; then
+    input=${input%$suffix}
+elif [[ $input == *"." ]]; then
+    input=${input%.}
+fi
+time g++ -Wall -g3 --std=c++20 -Wextra -pedantic -O0 -Wconversion -Wfloat-equal -fsanitize=address -DTIME -DFAST $input.cpp -o $input.sol
+time ./$input.sol <$input.txt
